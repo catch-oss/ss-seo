6c46b2a9fbcbee5e285357144fb1205dba2d5588
Az Ilari <az@dev-io.us>
2019-12-16 05:42:19 +1300

    ss-seop

--- _config.php
+++ _config.php
@@ -1,13 +1,1 @@
 <?php
-
-use SilverStripe\SiteConfig\SiteConfig;
-
-// // Ensure compatibility with PHP 7.2 ("object" is a reserved word),
-// // with SilverStripe 3.6 (using Object) and SilverStripe 3.7 (using SS_Object)
-// if (!class_exists('SS_Object')) class_alias('Object', 'SS_Object');
-
-// if (SiteConfig::config()->enable_seo_cache_lifetime == true) {
-//     SS_Cache::set_cache_lifetime('cacheblock', 28 * 24 * 60 * 60, 100);
-// }
-
-

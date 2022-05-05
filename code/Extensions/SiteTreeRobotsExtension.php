d5473a8b4b8f66f9d2af2d68181369d992484034
Az Ilari <az@dev-io.us>
2022-05-05 13:24:30 +1200

    robots options

---
+++ code/Extensions/SiteTreeRobotsExtension.php
@@ -0,0 +1,52 @@
+<?php
+namespace CatchDesign\SS\SEO\Extensions;
+
+use SilverStripe\ORM\DataExtension;
+use SilverStripe\Control\Controller;
+use SilverStripe\Forms\CheckboxSetField;
+use SilverStripe\Forms\FieldGroup;
+use SilverStripe\Forms\FieldList;
+
+/*
+ * Does a lookup on init at the request URL and does a 301 redirect to page link if they are not same
+ */
+
+class SiteTreeRobotsExtension extends DataExtension
+{
+    private static $db = array(
+        'RobotsTag' => 'Text'
+    );
+
+    public function updateSettingsFields(FieldList $fields) {
+        $fields->addFieldToTab(
+            'Root.Settings',
+            FieldGroup::create(
+                CheckboxSetField::create(
+                    'RobotsTag',
+                    'Options for this page',
+                    [
+                        'all' => 'all',
+                        'noindex' => 'noindex',
+                        'nofollow' => 'nofollow',
+                        'none' => 'none',
+                        'noarchive' => 'noarchive',
+                        'nositelinkssearchbox' => 'nositelinkssearchbox',
+                        'nosnippet' => 'nosnippet',
+                        'indexifembedded' => 'indexifembedded',
+                        'notranslate' => 'notranslate',
+                        'noimageindex' => 'noimageindex',
+                    ]
+                )->setDescription('See <a href="https://developers.google.com/search/docs/advanced/robots/robots_meta_tag" target="_blank">docs</a> for more info')
+            )->setTitle('Robots')
+        );
+        return $fields;
+    }
+
+    public function contentcontrollerInit()
+    {
+        $controller = Controller::curr();
+        $res = $controller->getResponse();
+        $val = preg_replace('/[^a-z,]/', '', $controller->data()->RobotsTag ?? 'all');
+        $res->addHeader('X-Robots-Tag', $val);
+    }
+}
